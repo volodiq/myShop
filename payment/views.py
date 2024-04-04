5c7b5a8241731b56b8066c45dfbd83109a99aa5e
rxskee <rxskee@gmail.com>
2024-04-04 17:35:26 +0300

    Refactor

--- payment/views.py
+++ payment/views.py
@@ -12,52 +12,50 @@ stripe.api_version = settings.STRIPE_API_VERSION
 
 
 def payment_process(request):
-    """Процесс оплаты пользователем заказа"""
-    order_id = request.session.get("order_id", None)
-    order = get_object_or_404(Order, pk=order_id)
+    order_id = request.session.get('order_id', None)
+    order = get_object_or_404(Order, id=order_id)
 
-    if request.method == "POST":
-        success_url = request.build_absolute_uri(reverse("payment:payment_success"))
-        cancel_url = request.build_absolute_uri(reverse("payment:payment_canceled"))
+    if request.method == 'POST':
+        success_url = request.build_absolute_uri(
+                        reverse('payment:payment_process'))
+        cancel_url = request.build_absolute_uri(
+                        reverse('payment:payment_canceled'))
 
-        # Данные платежа дял stripe
+        # Stripe checkout session data
         session_data = {
-            "mode": "payment",
-            "client_reference_id": order_id,
-            "success_url": success_url,
-            "cancel_url": cancel_url,
-            "line_items": [],
+            'mode': 'payment',
+            'client_reference_id': order.id,
+            'success_url': success_url,
+            'cancel_url': cancel_url,
+            'line_items': []
         }
-
-        # Заполнение позиций
         for item in order.items.all():
-            session_data["line_items"].append(
-                {
-                    "price_data": {
-                        "unit_amount": int(item.price * Decimal("100")),
-                        "currency": "rub",
-                        "product_data": {"name": item.product.name},
+            session_data['line_items'].append({
+                'price_data': {
+                    'unit_amount': int(item.price * Decimal('100')),
+                    'currency': 'rub',
+                    'product_data': {
+                        'name': item.product.name,
                     },
-                    "quantity": item.quantity,
-                }
-            )
+                },
+                'quantity': item.quantity,
+            })
 
-        # Создание сеанса оформления платежа stripe
-        session = stripe.checkout.Session.create(**session_data)
-
-        # купон Stripe
         if order.coupon:
             stripe_coupon = stripe.Coupon.create(
-                name=order.coupon.code, percent_off=order.discount, duration="once"
-            )
-            session_data["discounts"] = [
-                {"coupon": stripe_coupon.id}
-            ]  # создать сеанс оформления платежа Stripe
-            session = stripe.checkout.Session.create(**session_data)
-            # перенаправить к форме для платежа Stripe
-            return redirect(session.url, code=303)
-        else:
-            return render(request, "payment/process.html", locals())
+                                name=order.coupon.code,
+                                percent_off=order.discount,
+                                duration='once')
+            session_data['discounts'] = [{
+                'coupon': stripe_coupon.id
+            }]
+
+        session = stripe.checkout.Session.create(**session_data)
+
+        return redirect(session.url, code=303)
+
+    else:
+        return render(request, 'payment/process.html', locals())
 
 
 def payment_success(request):
